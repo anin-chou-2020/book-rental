84cac228f2d8412d10ddd4f503a4f8d74f4b6384
anin chou <aninchou2020.outlook.in>
2020-06-16 23:21:20 +0530

    added proxy config, rental detail

--- server/index.js
+++ server/index.js
@@ -6,15 +6,30 @@ const rental = require('./models/rental');
 const rentalRoutes = require('./routes/rentals');
 
 
-mongoose.connect(config.DB_URI).then(() => {
-    const fakeDb = new FakeDb();
-    fakeDb.seedDb();
-});
+// mongoose.connect(config.DB_URI).then(() => {
+//     const fakeDb = new FakeDb();
+//     fakeDb.seedDb();
+// });
+
+const url = 'mongodb://127.0.0.1:27017/book-rental'
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+        const fakeDb = new FakeDb();
+        fakeDb.seedDb();
+    });
+const db = mongoose.connection
+db.once('open', _ => {
+    console.log('Database connected:', url)
+})
+
+db.on('error', err => {
+    console.error('connection error:', err)
+})
+
 const app = express();
 
 app.use('/api/v1/rentals', rentalRoutes);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("abc");
 });
--- src/modules/rental/rental.module.ts
+++ src/modules/rental/rental.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+// import { NgPipesModule } from 'ngx-pipes';
+
+import { RentalRoutingModule } from './rental-routing.module';
+
+import { RentalService } from './services/rental.service';
+
 import { RentalComponent } from './components/rental/rental.component';
 import { RentalListComponent } from './components/rental-list/rental-list.component';
 import { RentalListItemComponent } from './components/rental-list-item/rental-list-item.component';
-import { RentalRoutingModule } from './rental-routing.module';
-import { RentalService } from './services/rental.service';
 import { RentalDetailComponent } from './components/rental-detail/rental-detail.component';
-
+import { UcwordsPipe } from './pipes/ucwords.pipe';
+import { UpperPipe } from './pipes/upper.pipe';
 @NgModule({
-  declarations: [RentalComponent, RentalListComponent, RentalListItemComponent, RentalDetailComponent],
-  imports: [CommonModule, RentalRoutingModule],
+  declarations: [RentalComponent, RentalListComponent, RentalListItemComponent, RentalDetailComponent, UcwordsPipe, UpperPipe],
+  imports: [CommonModule, HttpClientModule, RentalRoutingModule],
   providers: [RentalService]
 })
 export class RentalModule { }
